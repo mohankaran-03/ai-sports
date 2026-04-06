343a3975cf4b7de72899feac16a7d294749ea65e
pavanigopagoni <rishikagopagoni07@gmail.com>
2026-04-06 14:36:40 +0530

    fixed mediapipe compatibility

--- pose_engine.py
+++ pose_engine.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
-mp_pose = mp.solutions.pose
+mp_pose = mp.solutions.pose if hasattr(mp, "solutions") else None
 mp_drawing = mp.solutions.drawing_utils
 
 
