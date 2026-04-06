0729341fba143e46ca557a36280162048e5a7eb0
pavanigopagoni <rishikagopagoni07@gmail.com>
2026-04-06 14:25:02 +0530

    fixed port issue

--- app.py
+++ app.py
@@ -353,5 +353,5 @@ def delete_user(id):
 
 # -------- MAIN --------
 if __name__ == "__main__":
-    print("🚀 Starting Flask server...")
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
